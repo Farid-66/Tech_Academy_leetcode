e06badb37c5de3f4ba297676ea912f2808404af4
Farid-66 <70914203+Farid-66@users.noreply.github.com>
2021-10-13 21:02:14 +0300

    Leetcode_task1-task3

---
+++ main.js
@@ -0,0 +1,67 @@
+// // Task1
+
+// let arr=[4,3,2,1]
+// let str=""
+// let array=[]
+// for(let i=0;i<arr.length;i++){
+//     str=str+arr[i]
+// }
+
+// let num=parseInt(str)+1
+
+// while(num>0){
+//     let b=num%10
+//     array.unshift(b)
+//     num=parseInt(num/10)
+// }
+// console.log(array)
+
+// // -----
+
+
+
+
+
+
+// // Task2
+
+// let arr=[2,7,11,15] 
+// let target = 13
+// let count=0
+
+// for(let i=0;i<arr.length;i++){
+//     for(let j=0;j<arr.length;j++){
+//         if(arr[i]+arr[j]==target){
+//             console.log(i,j)
+//             count++
+//             break
+//         }
+//     }
+//     if(count>0){
+//         break
+//     }
+
+// }
+
+// // -----
+
+
+
+
+
+// // Task3
+
+// let arr1=[1,2]
+// let arr2=[3]
+// let add=0
+// let sum=0
+// for(let i=0;i<arr2.length;i++){
+//     add=arr2[i]
+//     arr1.push(add)  
+// }
+// for(let j=0;j<arr1.length;j++){
+//     sum=sum+arr1[j]
+// }
+// console.log(sum/arr1.length)
+
+// // -----
